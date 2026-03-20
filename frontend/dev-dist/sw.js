ba1c92a5be60bc25a0f5f121e09116b9175265fa
Danil <dain149521@gmail.com>
2026-03-20 13:41:38 +0200

    gitinore updater

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0d7dea0d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.npp4q5ga0rg"
+    "revision": "0.n6u0c8fjbug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
